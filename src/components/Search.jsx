b0ff8433d670941c23d2fb78029eda92a547815c
piyushsolanki1 <solankipiyush6262@gmail.com>
2025-08-17 12:07:43 +0530

    updated

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
 const Search = ({searchTerm, setSearchTerm}) => {
-  return (
-    <div className='search'>
-        <div>
-            <img src="src/assets/search.svg" alt="search" />
-            <input className='' type="text" placeholder='search animes' 
-            value={searchTerm}
-            onChange={(event)=> setSearchTerm(event.target.value)}/>
-        </div>
-    </div>
-  )
-}
+    return (
+      <div className='search'>
+          <div>
+              <img src="src/assets/search.svg" alt="search" />
+              <input className='' type="text" placeholder='search animes' 
+              value={searchTerm}
+              onChange={(event)=> setSearchTerm(event.target.value)}/>
+          </div>
+      </div>
+    )
+  }
 
 export default Search
